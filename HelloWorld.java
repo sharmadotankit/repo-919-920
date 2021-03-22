d22244dcaddcfe9366e19e9ef2afc6e95314a83c
sharmadotankit <81158505+sharmadotankit@users.noreply.github.com>
2021-03-22 16:41:57 +0530

    added add method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld{
   public static void main(String []args){
     System.out.println("Hello World");  
+    add(20,30);
+  }
+  
+  public static void add(int num1 , int num2){
+      System.out.println(num1+num2);
   }
 }
